d713589a01fca74b5921872091698284434c2aa7
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-10-11 02:16:50 +0200

    add smoothe scrolling

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,10 @@
 function moveRight() {
     const ourWorkContainer = document.getElementById("ourWorkContainer");
     const scrollValue = ourWorkContainer.scrollLeft;
-    ourWorkContainer.scrollLeft += 330;
+    ourWorkContainer.scrollLeft += 290;
 };
 
 function moveLeft() {
     const ourWorkContainer = document.getElementById("ourWorkContainer");
-    ourWorkContainer.scrollLeft -= 320;
+    ourWorkContainer.scrollLeft -= 290;
 }
