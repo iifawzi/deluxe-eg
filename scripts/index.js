216b9bd6d3a4667f321d617fd26b148bad476119
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-10-12 02:36:27 +0200

    improvments to js

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,13 @@
-
-function moveRight() {
+function move(dir) {
     const ourWorkContainer = document.getElementById("ourWorkContainer");
+    if (dir === "right"){
     ourWorkContainer.scrollTo(ourWorkContainer.scrollLeft += 290,0);
-};
-
-function moveLeft() {
-    const ourWorkContainer = document.getElementById("ourWorkContainer");
+    }else {
+    let remainingToNearest = ourWorkContainer.scrollLeft % 290;
+    if (remainingToNearest === 0){
     ourWorkContainer.scrollTo(ourWorkContainer.scrollLeft -= 290,0);
+    }else {
+        ourWorkContainer.scrollTo(ourWorkContainer.scrollLeft -= remainingToNearest,0);
+    }
+    }
 }
